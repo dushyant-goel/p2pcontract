086612ebfa4a9c71f35d66a325a0525ebff3771e
Dushyant Goel <its.dushyant.goel@gmail.com>
2026-04-28 00:38:53 +0100

    added all assert statements, polishing delights.

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ To create the test for "createOffer" function of MansaTrade.sol
 # Run
 `npx hardhat test --network localhost test/MansaTrade.ts`
 
-
 (at `../contract/hardhat/`)
+
+# Issues with createOffer 
+(Responsibility of .sol file)
+- Add check for `min_amount` < `token_amount` < `max_amount`
--- contract/hardhat/test/MansaTrade.ts
+++ contract/hardhat/test/MansaTrade.ts
@@ -2,7 +2,29 @@ import assert from "node:assert/strict";
 import { describe, it } from "node:test";
 
 import hre from "hardhat";
+import { create } from "node:domain";
 const { viem, networkHelpers } = await hre.network.create();
+
+// Define the structure as it exists in Solidity
+interface Offer {
+    owner: `0x${string}`;
+    token_address: `0x${string}`;
+    fiat: string;
+    rate: string;
+    payment_options: string;
+    public_key: string;
+    offer_terms: string;
+    token_amount: bigint;
+    min_limit: bigint;
+    max_limit: bigint;
+    bought: bigint;
+    created_at: bigint;
+    offer_index: bigint;
+    time_limit: number;
+    status: boolean;
+    eth: boolean;
+}
+
 
 describe("MansaTrade Escrow Mechanics", function () {
 
@@ -21,36 +43,93 @@ describe("MansaTrade Escrow Mechanics", function () {
         // construct the offer
 
         const txHash = await mansaTrade.write.createOffer([
-          "0x0000000000000000000000000000000000000000",  // address token_address,
-          "GBP",                                        // string memory fiat, 
-          "2500",                                       // string memory rate, 
+          "0x0000000000000000000000000000000000000000",  // address token_address
+          "GBP",                                        // string memory fiat 
+          "2500",                                       // string memory rate 
           "Bank Transfer",                              // string memory payment_options, 
           "mock_public_key",                            // string memory public_key, 
           "prompt settlement expected.",                // string memory offer_terms, 
-          30,                                           // uint8 time_limit, 
-          true,                                         // bool eth, -> first pass testing
+          30,                                           // uint8 time_limit
+          true,                                         // bool eth
           10000000000000000000n,                        // uint256 token_amount, 
-          1000000000000000000n,                         // uint256 min_limit, 
+          1000000000000000000n,                         // uint256 min_limit
           10000000000000000000n                         // uint256 max_limit 
         ])
+        await publicClient.waitForTransactionReceipt({hash: txHash})
         
-        const createdOffer = await mansaTrade.read.getOfferByIndex([0n]);
+        const createdOffer = await mansaTrade.read.getOfferByIndex([0n]) as Offer;
+
         console.log(`Offer created. Transaction Hash: ${txHash}`);
         
+        // Tests!
+
+        assert.equal(
+            createdOffer.owner.toLocaleLowerCase(),
+            owner.account.address.toLocaleLowerCase(),
+            "The offer owner should match the wallet that created it."
+        );
+
+        assert.equal(
+            createdOffer.public_key,
+            "mock_public_key",
+            "The offer public key should match as input."
+        );
+
+        assert.equal(
+            createdOffer.eth,
+            true,
+            "The offer should be flagged for native ETH, as input."
+        );
+
+        assert.equal(
+            createdOffer.fiat, 
+            "GBP", 
+            "The offer fiat currency should match as input per owner's desired fiat."
+        );
+
+        assert.equal(
+            createdOffer.rate,
+            "2500",
+            "The offer rate should match as input per owner's offered rate."
+        );
+
+        assert.equal(
+            createdOffer.token_amount,
+            10000000000000000000n,
+            "The token amount should match as input per owner's offer of 10 ETH."
+        );
+
+        assert.equal(
+            createdOffer.payment_options,
+            "Bank Transfer",
+            "The offer payment options should match as input."
+        );
         
+        assert.equal(
+            createdOffer.offer_terms,
+            "prompt settlement expected.",
+            "The offer terms should match as input."
+        );
+
+        assert.equal(
+            createdOffer.time_limit,
+            30,
+            "The offer time limit should match as input."
+        );
+
+        assert.equal(
+            createdOffer.min_limit,
+            1000000000000000000n,
+            "The offer min limit should match as input per owner's 1 ETH limit."
+        );
+
+        assert.equal(
+            createdOffer.max_limit,
+            10000000000000000000n,
+            "The offer max limit should match as input per owner's 10 ETH limit."
+        );
     
     });
 })
 
 
-// async function main() {
-
-//     const walletClients = await hre.viem.getWalletClients();
-//     const addresses = accounts.map(client => client.account.address);
-
-//     console.log("Available accounts: ", addresses);
-
-//     const [owner, otherAccount] =  await hre.viem.getWalletClients();
-// }
-
-// main();
